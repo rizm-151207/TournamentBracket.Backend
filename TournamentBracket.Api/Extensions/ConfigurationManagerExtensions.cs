0bd77b0f10afbbb0b722cd43a9630215b644af53
Kirill Masalov <masalov.kirill03@mail.ru>
2026-01-15 21:28:14 +0500

    Add db secrets to docker

---
+++ TournamentBracket.Api/Extensions/ConfigurationManagerExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace TournamentBracket.Api.Extensions;
+
+public static class ConfigurationManagerExtensions
+{
+    private const string DbUserVariableName = "TOURNAMENT_DB_USER";
+    private const string DbPasswordVariableName = "TOURNAMENT_DB_PASSWORD";
+    
+    public static string? GetConnectionStringWithEnvironmentVariables(this IConfigurationManager manager, string key)
+    {
+        return manager.GetConnectionString(key)?
+            .Replace($"${{{DbUserVariableName}}}", Environment.GetEnvironmentVariable(DbUserVariableName))
+            .Replace($"${{{DbPasswordVariableName}}}", Environment.GetEnvironmentVariable(DbPasswordVariableName));
+    }
+}
