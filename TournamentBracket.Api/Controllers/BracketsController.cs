2efb92d7bde58ab632098813ddc7f82a9ddf7d66
Kirill Masalov <masalov.kirill03@mail.ru>
2026-01-26 22:57:25 +0500

    Add BracketsController

---
+++ TournamentBracket.Api/Controllers/BracketsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentBracket.Application.Brackets.Interfaces;
+using TournamentBracket.Domain.Brackets;
+
+namespace TournamentBracket.Api.Controllers;
+
+[ApiController]
+[Route("api/brackets")]
+public class BracketsController : ExtendedControllerBase
+{
+    private readonly ITournamentBracketsService tournamentBracketsService;
+
+    public BracketsController(
+        ITournamentBracketsService tournamentBracketsService)
+    {
+        this.tournamentBracketsService = tournamentBracketsService;
+    }
+
+    [HttpGet("{bracketType}/{bracketId}")]
+    public async Task<IActionResult> GetBracket([FromRoute] Guid bracketId, [FromRoute] BracketType bracketType)
+    {
+        var result = await tournamentBracketsService.GetBracket(bracketId, bracketType);
+        return ToActionResult(result);
+    }
+}
