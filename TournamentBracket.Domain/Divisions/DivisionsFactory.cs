a8f89f67e8251a7273007fce28d51691d0cd4385
Kirill Masalov <masalov.kirill03@mail.ru>
2026-02-01 22:21:50 +0500

    Fix adding to division

--- TournamentBracket.Domain/Divisions/DivisionsFactory.cs
+++ TournamentBracket.Domain/Divisions/DivisionsFactory.cs
@@ -6,18 +6,18 @@ public class DivisionsFactory
     {
         var divisions = new List<Division>
         {
-            new(competitionId, false, maxAge: Division.MinDivisionAge),
-            new(competitionId, true, maxAge: Division.MinDivisionAge)
+            new(competitionId, false, maxAge: Division.MinDivisionAge - 1),
+            new(competitionId, true, maxAge: Division.MinDivisionAge - 1)
         };
 
         for (var min = Division.MinDivisionAge; min < Division.MaxKidAge; min += Division.KidAgeStep)
         {
-            divisions.Add(new(competitionId, false, minAge: min, maxAge: min + Division.KidAgeStep));
-            divisions.Add(new(competitionId, true, minAge: min, maxAge: min + Division.KidAgeStep));
+            divisions.Add(new(competitionId, false, minAge: min, maxAge: min + Division.KidAgeStep - 1));
+            divisions.Add(new(competitionId, true, minAge: min, maxAge: min + Division.KidAgeStep - 1));
         }
 
-        divisions.Add(new(competitionId, false, minAge: Division.MaxKidAge, maxAge: Division.VeteranAge));
-        divisions.Add(new(competitionId, true, minAge: Division.MaxKidAge, maxAge: Division.VeteranAge));
+        divisions.Add(new(competitionId, false, minAge: Division.MaxKidAge, maxAge: Division.VeteranAge - 1));
+        divisions.Add(new(competitionId, true, minAge: Division.MaxKidAge, maxAge: Division.VeteranAge - 1));
         divisions.Add(new(competitionId, false, minAge: Division.VeteranAge));
         divisions.Add(new(competitionId, true, minAge: Division.VeteranAge));
 
